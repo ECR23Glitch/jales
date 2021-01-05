2ba0cc8f886a4aba0af921ef138392723687efb5
Elias2 <ecr0799@gmail.com>
2021-01-04 20:21:05 -0600

    Registro exitoso

--- assets/js/register-user.js
+++ assets/js/register-user.js
@@ -1,40 +1,11 @@
 $('document').ready(function () {
-
-    $('#bnext1').on('click', function(){
+    $('#bnext3').on('click', function(){
         // XMLHttpRequest
         var url = "assets/php/registro-userPT1.php";
           $.ajax({
              type: "POST",
              url: url,
-             data: $("#registro1").serialize(),
-             success: function(data)
-             {
-               alert('Primera parte completada');
-             }
-         });
-    });
-
-    $('#bnext2').on('click', function(){
-        // XMLHttpRequest
-        var url = "assets/php/registro-userPT2.php";
-          $.ajax({
-             type: "POST",
-             url: url,
-             data: $("#registro2").serialize(),
-             success: function(data)
-             {
-               alert('Segunda parte completada');
-             }
-         });
-    });
-
-    $('#bnext3').on('click', function(){
-        // XMLHttpRequest
-        var url = "assets/php/registro-userPT3.php";
-          $.ajax({
-             type: "POST",
-             url: url,
-             data: $("#registro3").serialize(),
+             data: $("#registro").serialize(),
              success: function(data)
              {
                alert('Se ha finalizado tu registro');
