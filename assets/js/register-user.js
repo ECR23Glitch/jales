9186604907409f3ac2d13373691442e0566b73df
Elias2 <ecr0799@gmail.com>
2021-01-05 21:05:39 -0600

    Validaciones

--- assets/js/register-user.js
+++ assets/js/register-user.js
@@ -1,14 +1,32 @@
 $('document').ready(function () {
     $('#bnext3').on('click', function(){
         // XMLHttpRequest
-        var url = "assets/php/registro-userPT1.php";
+        event.preventDefault();
+        var url = "assets/php/registro-user.php";
           $.ajax({
              type: "POST",
              url: url,
              data: $("#registro").serialize(),
              success: function(data)
              {
-               alert('Se ha finalizado tu registro');
+               toastr["success"]("Bienvenido", "Se ha completado tu registro");
+               toastr.options = {
+                        "closeButton": true,
+                        "debug": false,
+                        "newestOnTop": false,
+                        "progressBar": false,
+                        "positionClass": "toast-top-center",
+                        "preventDuplicates": false,
+                        "onclick": $(location).attr('href','login.html'),
+                        "showDuration": "3000",
+                        "hideDuration": "1000",
+                        "timeOut": "5000",
+                        "extendedTimeOut": "1000",
+                        "showEasing": "swing",
+                        "hideEasing": "linear",
+                        "showMethod": "fadeIn",
+                        "hideMethod": "fadeOut"
+                      }
              }
          });
     });
