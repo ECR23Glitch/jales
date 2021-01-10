e58da7de924db2ee4c11a5cd9b5eadacc99bf711
marina-101098 <i17220613.19@puebla.tecnm.mx>
2021-01-09 22:37:00 -0600

    Se coloco el ícono

--- profile_user.php
+++ profile_user.php
@@ -7,6 +7,7 @@
   <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+      <link rel="icon" type="image/png" href="assets/img/Logo/color.png">
       <title>Mi perfil</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
       <!--Iconos - Estrellas-->
